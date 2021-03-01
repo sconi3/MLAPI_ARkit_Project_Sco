94ed338262a0e60b3a63f3539c0b91a503fca4b0
nn <najjatujajaw@gmail.com>
2021-03-01 20:17:17 +0800

    202103012001 1.增加VFX特效示例 烟花 2.MLAPI多角色同步 移动 3.Unity 普通粒子示例

---
+++ MLAPI_ARkit_Project_Sco/Assets/_Main/Script/MenuScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAPI;
+
+public class MenuScript : MonoBehaviour
+{
+    public GameObject menuPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Host()
+    {
+        NetworkingManager.Singleton.StartHost();
+        menuPanel.SetActive(false);
+    }
+    public void Join()
+    {
+        NetworkingManager.Singleton.StartClient();
+        menuPanel.SetActive(false);
+    }
+}
