29351cf3339242684582ec36378b4807887354e4
nn <najjatujajaw@gmail.com>
2021-03-07 22:01:24 +0800

    20210307 1.网络同步例子完善 2.网络数值同步例子完善
    InvokeServerRpc(Shoot); //创建同步函数
        [ServerRPC] //同步接收
        private void Shoot()
    https://mlapi.network/wiki/messaging-system/
    //同步VAR
        NetworkedVarBool shooting = new NetworkedVarBool(new NetworkedVarSettings {
            WritePermission = NetworkedVarPermission.OwnerOnly
        }, false);
    https://mlapi.network/wiki/networkedvar/

---
+++ MLAPI_ARkit_Project_Sco_clone/Assets/_Main/Script/PlayerHealth.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAPI;
+using MLAPI.NetworkedVar;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public  NetworkedVarFloat health = new NetworkedVarFloat(100f);
+
+    
+    public void TakeDamage(float damage)
+    {
+        health.Value -= damage;
+    } 
+}
---
+++ MLAPI_ARkit_Project_Sco_clone/Assets/_Main/Script/PlayerShooting.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAPI;
+using MLAPI.NetworkedVar;
+using MLAPI.Messaging;
+using System;
+
+public class PlayerShooting : NetworkedBehaviour
+{
+    public ParticleSystem bulletParticleSystem;
+    private ParticleSystem.EmissionModule em;
+
+    NetworkedVarBool shooting = new NetworkedVarBool(new NetworkedVarSettings { 
+        WritePermission = NetworkedVarPermission.OwnerOnly
+    }, false);
+    //bool shooting = false;
+    float fireRate = 10f;
+    float shootTimer = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        em = bulletParticleSystem.emission;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsLocalPlayer)
+        {
+            shooting.Value = Input.GetMouseButton(0);
+            shootTimer += Time.deltaTime;
+            if (shooting.Value && shootTimer >= 1f/fireRate)
+            {
+                shootTimer = 0;
+                //call our method
+                InvokeServerRpc(Shoot);
+            }
+        }
+
+        em.rateOverTime = shooting.Value ? 10f : 0f;
+
+    }
+    [ServerRPC]
+    private void Shoot()
+    {
+        Ray ray  = new Ray(bulletParticleSystem.transform.position,bulletParticleSystem.transform.forward);
+        if(Physics.Raycast(ray,out RaycastHit hit, 100f))
+        {
+            //we hit something
+            var player = hit.collider.GetComponent<PlayerHealth>();
+            if(player != null)
+            {
+                //we hit a player
+                player.TakeDamage(10f);
+            }
+        }
+    }
+}
